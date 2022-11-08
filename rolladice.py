635a2860ff7e8013f4bd91557750d62445066005
aa195 <78353803+aa195@users.noreply.github.com>
2022-11-08 19:07:29 +0530

    Add files via upload

---
+++ rolladice.py
@@ -0,0 +1,29 @@
+import random
+
+response = input("press y to start and n to exit")
+while response == "y":
+ response = input("press y to start and n to exit")
+ a = random.randint(1,6)
+
+ if a == 1 :
+      print("[0]")
+
+ elif a == 2 :
+     print("[0]","","[0]")
+
+ elif a == 3 :
+     print("[0]","","[0]","","[0]")
+
+ elif a == 4 :
+     print("[0]","","[0]")
+     print("[0]","","[0]")
+
+ elif a == 5 :
+        print("[0]","","[0]")
+        print(" ","[0]","")
+        print("[0]","","[0]")
+
+ else :
+       print("[0]","","[0]")
+       print("[0]","","[0]")
+       print("[0]","","[0]")
